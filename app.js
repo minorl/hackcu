a36c1564cd632751080b27ccffca0d18249adb79
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2015-04-10 20:00:59 -0600

    Moved the js folder to public, added the base html page and the server

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+var _ = require('lodash');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
+
+app.set('port', (process.env.PORT || 3000));
+
+var server = app.listen(app.get('port'), function() {
+
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('App listening at http://%s:%s', host, port);
+});
+
