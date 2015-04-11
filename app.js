97bc1d543c2ca8bd41d909ef2d8cf2b14e1a745a
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2015-04-10 21:31:25 -0600

    Added a dummy connection between server and client for testing purposes

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ var app = express();
 
 var _ = require('lodash');
 
+var Eureca = require('eureca.io');
+
+var eurecaServer = new Eureca.Server();
+
 app.use(express.static(__dirname + '/public'));
+
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
 
 app.get('/', function(req, res) {
     res.render('index.html');
@@ -18,3 +24,14 @@ var server = app.listen(app.get('port'), function() {
     console.log('App listening at http://%s:%s', host, port);
 });
 
+//attache eureca.io to http server
+eurecaServer.attach(server);
+
+//detect client connection
+eurecaServer.onConnect(function (conn) {
+    console.log('New client id=%s', conn.id, conn.remoteAddress);
+});
+
+eurecaServer.exports.hello = function () {
+    console.log('Hello from client');
+}
