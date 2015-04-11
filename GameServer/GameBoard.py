4a218bc5aca96d91581182eb5e294f2be8a8c63f
eroncevich <eroncevich@gmail.com>
2015-04-10 22:54:00 -0600

    list of hard code added

--- GameServer/GameBoard.py
+++ GameServer/GameBoard.py
@@ -1,27 +1,37 @@
-class GameBoard:
+class GameBoard(object):
 	def __init__(self):
 		self.corners = []
 		self.edges = {} #(id1,id2)->CornerEdge
+		self.cornerTbl = {0:(1,8,None),1:(None,2,0),2:(3,10,1),3:(None,4,2),4:(5,12,3),5:(None,6,4),6:(None,14,5),
+		7:(8,17,None),8:(0,9,7),9:(10,19,8),10:(11,2,9),11:(12,21,10),12:(4,13,11),13:(14,23,12),14:(6,15,13),15:(None,25,14),
+		16:(17,27,None),17:(7,18,16),18:(19,29,17),19:(9,20,18),20:(21,31,19),21:(11,22,20),22:(23,33,21),23:(13,24,22),24:(25,35,23),25:(15,26,24),26:(None,37,25),
+		27:(16,28,None),28:(29,38,27),29:(18,30,28),30:(31,40,29),31:(20,32,30),32:(33,42,31),33:(22,34,32),34:(35,44,33),35:(24,36,34),36:(37,46,35),37:(26,None,36),
+		38:(28,39,None),39:(40,47,38),40:(30,41,39),41:(42,49,40),42:(32,43,41),43:(44,51,42),44:(34,45,43),45:(46,53,44),46:(36,None,45),
+		47:(39,48,None),48:(49,None,47),49:(41,50,48),50:(51,None,49),51:(43,52,50),52:(53,None,51),53:(45,None,52)}
+
+
 	pass
 
 
-class Corner:
+class Corner(object):
 	def __init__(self,nodeID):
 		self.nodeID = nodeID
 	def setEdges(self):
+		pass
+	def setTiles(self):
 		pass
 
 	def addBuilding(playerID, buildingType):
 		pass
 	pass
 
-class Tile:
+class Tile(object):
 	def __init__(self,tileID, resource,number):
 		self.tileID = tileID
 		self.resource = resource
 		self.number = number
 
-class CornerEdge:
+class CornerEdge(object):
 	def __init__(self,id1, id2):
 		self.hasRoad = False
 		self.corners = (id1, id2)
@@ -29,6 +39,9 @@ class CornerEdge:
 	def addRoad(self, playerID):
 		self.hasRoad = True
 		self.playerID = playerID
+
+board = GameBoard()
+print board.cornerTbl
 
 edge = CornerEdge(0,3)
 print edge.hasRoad, edge.playerID
