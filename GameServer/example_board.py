1b622bb9bf91d472256266b086c6c5e7880dc9b0
eroncevich <eroncevich@gmail.com>
2015-04-11 23:00:30 -0600

    step example

--- GameServer/example_board.py
+++ GameServer/example_board.py
@@ -2,6 +2,7 @@ from view_updater import *
 from Validator import *
 from GameState import *
 from Move import *
+import time
 
 updater = ViewUpdater()
 gs = GameState(4)
@@ -9,29 +10,64 @@ updater.sendTiles(gs)
 
 board = gs.board
 board.addRoad(4,12,0)
+updater.sendGameState(gs)
+time.sleep(1)
 corner1 = board.corners[12]
 corner1.addBuilding(0,"settlement")
+updater.sendGameState(gs)
+time.sleep(1)
 board.addRoad(12,13,0)
+updater.sendGameState(gs)
+time.sleep(1)
 board.addRoad(13,23,0)
+updater.sendGameState(gs)
+time.sleep(1)
 
 board.addRoad(23,24,0)
+updater.sendGameState(gs)
+time.sleep(1)
 board.addRoad(24,25,0)
+updater.sendGameState(gs)
+time.sleep(1)
 board.addRoad(24,35,0)
+updater.sendGameState(gs)
+time.sleep(1)
 
 board.addRoad(35,36,0)
+updater.sendGameState(gs)
+time.sleep(1)
 board.addRoad(35,34,0)
+updater.sendGameState(gs)
+time.sleep(1)
 corner2 = board.corners[34]
 corner2.addBuilding(0,"settlement")
+updater.sendGameState(gs)
+time.sleep(1)
 board.addRoad(34,33,0)
+updater.sendGameState(gs)
+time.sleep(1)
 
 board.addRoad(33,32,1)
+updater.sendGameState(gs)
+time.sleep(1)
 board.addRoad(32,31,1)
+updater.sendGameState(gs)
+time.sleep(1)
 board.addRoad(31,20,1)
+updater.sendGameState(gs)
+time.sleep(1)
 board.addRoad(20,21,1)
+updater.sendGameState(gs)
+time.sleep(1)
 board.addRoad(21,22,1)
+updater.sendGameState(gs)
+time.sleep(1)
 board.addRoad(22,23,1)
+updater.sendGameState(gs)
+time.sleep(1)
 corner3 = board.corners[23]
 corner3.addBuilding(1,"settlement")
+updater.sendGameState(gs)
 
 #board.getLongestRoad()
 gs.players[0].resources['brick']=1
@@ -54,7 +90,7 @@ print check.validateMove(move1)
 print check.validateMove(move2)
 
 
-updater.sendGameState(gs)
+
 
 #print gs.board.setRobber('A')
 #print len(board.edges)
