634cb0a53770746210f7dcaa43d99cdfc015fe43
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2015-04-12 02:54:28 -0600

    Added robber movement

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -38,6 +38,10 @@ var total_y_offset = 25;
 
 var texts = {};
 
+var curr_robber;
+
+var tile_node_lookup = {};
+
 // Variable to store the action after having clicked on an action button
 // -1 => no action, 0 => build road, 1 => build settlement, 2 => build city
 var action = -1;
@@ -227,9 +231,14 @@ function drawTile(x, y, tile_node) {
         dieLayer.add(robber);
         robber.scale.x = 0.5;
         robber.scale.y = 0.5;
+
+        curr_robber = { "robber": robber, "id": tile_node.id };
     }
 
     tiles.push(sprite);
+    var index = tiles.length;
+
+    tile_node_lookup[tile_node.id] = { "index": index, "x": x + 45, "y": y + 60};
     tileLayer.add(sprite);
     sprite.scale.x = 0.75;
     sprite.scale.y = 0.75;
@@ -283,6 +292,19 @@ function drawPieces(data) {
             drawRoad(x1, y1, x2, y2, players[edge_state.player_id]);
         }
     });
+
+    if (data.robber != curr_robber.id) {
+        curr_robber.robber.kill();
+        var robber_info = tile_node_lookup[data.robber];
+        x = robber_info.x;
+        y = robber_info.y;
+        var robber = new Phaser.Sprite(game, x, y, "robber");
+        robber.scale.x = 0.5;
+        robber.scale.y = 0.5;
+        dieLayer.add(robber);
+
+        curr_robber = { "robber": robber, "id": data.robber };
+    }
 
     var i = 0;
     data.player_states.forEach(function (player_state) {
