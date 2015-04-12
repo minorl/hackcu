76c7feedf5a18a4de6f6f02894b9149a03609183
eroncevich <eroncevich@gmail.com>
2015-04-11 22:29:43 -0600

    Example code

---
+++ GameServer/example_board.py
@@ -0,0 +1,57 @@
+from view_updater import *
+from Validator import *
+from GameState import *
+from Move import *
+
+gs = GameState(4)
+board = gs.board
+board.addRoad(4,12,0)
+corner1 = board.corners[12]
+corner1.addBuilding(0,"settlement")
+board.addRoad(12,13,0)
+board.addRoad(13,23,0)
+
+board.addRoad(23,24,0)
+board.addRoad(24,25,0)
+board.addRoad(24,35,0)
+
+board.addRoad(35,36,0)
+board.addRoad(35,34,0)
+corner2 = board.corners[34]
+corner2.addBuilding(0,"settlement")
+board.addRoad(34,33,0)
+
+board.addRoad(33,32,1)
+board.addRoad(32,31,1)
+board.addRoad(31,20,1)
+board.addRoad(20,21,1)
+board.addRoad(21,22,1)
+board.addRoad(22,23,1)
+corner3 = board.corners[23]
+corner3.addBuilding(1,"settlement")
+
+#board.getLongestRoad()
+gs.players[0].resources['brick']=1
+gs.players[0].resources['wood']=1
+gs.players[0].resources['wheat']=1
+gs.players[0].resources['sheep']=1
+check = Validator(gs)
+gs.phase = 'standard'
+move1 = Move(0,'build',{'structure':'settlement', 'location':25})
+
+
+gs.players[1].resources['ore']=3
+gs.players[1].resources['wood']=1
+gs.players[1].resources['wheat']=2
+gs.players[1].resources['sheep']=1
+check = Validator(gs)
+move2 = Move(1,'build',{'structure':'city', 'location':34})
+
+print check.validateMove(move1)
+print check.validateMove(move2)
+
+updater = ViewUpdater()
+print updater.sendJSON(gs)
+
+#print gs.board.setRobber('A')
+#print len(board.edges)
