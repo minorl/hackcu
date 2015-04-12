bd5965b14f9fb24b8e4970a01750676debf80c54
Leslie Minor <minorl@colorado.edu>
2015-04-12 03:52:02 -0600

    Sleep before starting new game

--- gamedriver.py
+++ gamedriver.py
@@ -1,7 +1,7 @@
 from GameServer.controller import Controller
 from mlprototype.aiplayer import AIPlayer
 from GameServer.view_updater import ViewUpdater
-
+import time
 P1 = AIPlayer()
 P2 = AIPlayer()
 P3 = AIPlayer()
@@ -11,4 +11,4 @@ update = ViewUpdater()
 while True:
     c = Controller([P1, P2, P3, P4], update)
     c.play()
-
+    time.sleep(5)
